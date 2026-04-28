bdf6fd12df88095abe0af3632e78e3d50776b575
Jean-Luc Kouassi <smith00547986@gmail.com>
2026-04-28 18:58:11 +0000

    Initialisation de BankAccount, classe parent et construction du compte bancaire

---
+++ src/BankAccount.java
@@ -0,0 +1,13 @@
+public class BankAccount {
+    //Classe parente représentant un compte bancaire de base.
+        String account;
+        double balance;
+
+        //Constructeur du compte bancaire.
+
+        public BankAccount(String account, double balance) {
+            this.account = account;
+            this.balance = balance;
+        }
+
+}
