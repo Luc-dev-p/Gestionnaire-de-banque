aff78fdd6e4633eadda2cd2224fa41d7167b76f0
Jean-Luc Kouassi <smith00547986@gmail.com>
2026-04-28 20:48:38 +0000

    Définition des différents paramètres en ligne de comptes

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,2 +1,8 @@
-public class SavingsAccount {
+public class SavingsAccount extends BankAccount {
+    double interestRate;
+
+    public SavingsAccount(String account, double balance, double interestRate) {
+        super(account, balance);
+        this.interestRate = interestRate;
+    }
 }
